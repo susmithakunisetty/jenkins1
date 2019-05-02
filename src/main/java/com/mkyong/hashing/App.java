bf2b8492617c9ceb81b8e750d114fbb1bc642bdc
susmitha <susmithakunisetty@gmail.com>
2019-05-02 04:11:21 -0700

    Initial commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,27 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+public boolean isPrime(int n){
+		boolean val = true;
+for(int i=2; i<=n/2;i++){
+	if(n%2 == 0){
+		val = false;
+		break;
+	}
+
+}
+return val;
+}
+
+
+}
---
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -0,0 +1,56 @@
+package com.mkyong.hashing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+	
+public void testPrime1()
+    {
+	boolean b1 = new App().isPrime(5);
+        assertTrue( b1);
+    }
+
+public void testPrime2()
+    {
+	boolean b1 = new App().isPrime(9);
+        assertTrue( b1);
+    }
+
+public void testPrime3()
+    {
+	boolean b1 = new App().isPrime(7);
+        assertTrue( b1);
+    }
+}
